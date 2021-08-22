80312de2fa7b4c76af9eeb31b8ae32210a29d351
Joshua <favrejoshua@yahoo.com>
2021-08-21 21:46:13 -0500

    sets views to get ticker info or display message

--- stonks/quotes/views.py
+++ stonks/quotes/views.py
@@ -3,18 +3,23 @@ from django.shortcuts import render
 
 def home(request):
     import requests
-    import json 
+    import json
 
-    # pk_d8c04f5d4e1c4b39b7fcc939f540a081
-    api_request = requests.get(
-        "https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_d8c04f5d4e1c4b39b7fcc939f540a081")
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error..."
+        api_request = requests.get(
+            "https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_d8c04f5d4e1c4b39b7fcc939f540a081")
 
-    return render(request, 'home.html', {'api': api})
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+        return render(request, 'home.html', {'api': api})
+
+    else:
+        return render(request, 'home.html', {'ticker': "Enter a Ticker Symbol Above..."})
+
 
 def about(request):
     return render(request, 'about.html', {})
