f6d694fc4ddccd9b0954d2bc2aa10673d478b270
Joshua <favrejoshua@yahoo.com>
2021-08-23 10:48:06 -0500

    add form to post stock to views and form html to add_stock, also create forms.py

--- stonks/quotes/views.py
+++ stonks/quotes/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Stock
+from .forms import StockForm
+from djangocontrib import messages
+
 
 
 def home(request):
@@ -27,6 +30,15 @@ def about(request):
 
 
 def add_stock(request):
+    if request.method == 'POST':
+        form = StockForm(request.POST or None)
 
-    ticker = Stock.objects.all()
-    return render(request, 'add_stock.html', {'ticker': ticker})
+    if form.is_valid():
+        form.save()
+        message.success(request, ("Stock Has Been Added!"))
+        return redirect('add_stock')
+
+    else:
+
+        ticker = Stock.objects.all()
+        return render(request, 'add_stock.html', {'ticker': ticker})
